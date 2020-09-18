a5c4c0bd339642ff3c7d61420caa480bb1cd1b1c
rom18009 <71232833+rom18009@users.noreply.github.com>
2020-09-18 17:21:22 -0600

    Update currentYear.js

--- js/currentYear.js
+++ js/currentYear.js
@@ -1,3 +1,9 @@
-var d = new Date();
-a = d.getFullYear(); 
-document.getElementById('currentYear').innerHtml = a;
+  // Creating Date Object 
+  var dateobj = new Date(); 
+  
+  // year from above object 
+  // is being fetched using getFullYear(). 
+  var B = dateobj.getFullYear(); 
+    
+  // Printing current year 
+  document.write(B); 
