999f0742e00b24c797b5521b7e185674158a8100
rom18009 <rom18009@byui.edu>
2020-10-03 23:47:28 -0600

    updated small.css and styling

---
+++ lesson3/js/currentyear.js
@@ -0,0 +1,9 @@
+  // Creating Date Object 
+  var dateobj = new Date(); 
+  
+  // year from above object 
+  // is being fetched using getFullYear(). 
+  var B = dateobj.getFullYear(); 
+    
+  // Printing current year 
+  document.write(B); 
