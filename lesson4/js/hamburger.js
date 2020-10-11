c07ef75a17ad969e1b5127775de9905457504dbc
rom18009 <rom18009@byui.edu>
2020-10-10 23:58:16 -0600

    preston-page upload

---
+++ lesson4/js/hamburger.js
@@ -0,0 +1,6 @@
+const hambutton = document.querySelector('.menu');
+const mainnav = document.querySelector('.clearfix')
+
+hambutton.addEventListener('click', () => {mainnav.classList.toggle('responsive')}, false);
+
+window.onresize = () => {if (window.innerWidth > 760) mainnav.classList.remove('responsive')};
