c9edb4740b5ba45d7e22c3d991dadfc9416b3136
rom18009 <rom18009@byui.edu>
2020-10-12 11:19:38 -0600

    corrections to lesson3

--- lesson4/js/hamburger.js
+++ lesson4/js/hamburger.js
@@ -1,6 +1,10 @@
 const hambutton = document.querySelector('.menu');
 const mainnav = document.querySelector('.clearfix')
 
-hambutton.addEventListener('click', () => {mainnav.classList.toggle('responsive')}, false);
+hambutton.addEventListener('click', () => {
+    mainnav.classList.toggle('responsive')
+}, false);
 
-window.onresize = () => {if (window.innerWidth > 760) mainnav.classList.remove('responsive')};
+window.onresize = () => {
+    if (window.innerWidth > 760) mainnav.classList.remove('responsive')
+};
