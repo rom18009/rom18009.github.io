7a6e6d949dc8077c815214cf572c42f3573b6066
rom18009 <rom18009@byui.edu>
2020-11-07 16:20:37 -0700

    Storm Center Page

--- lesson8/js/hamburger.js
+++ lesson8/js/hamburger.js
@@ -2,7 +2,7 @@ function menuToggle() {
     document.getElementsByClassName("clearfix")[0].classList.toggle("responsive");
 
     if (document.getElementById("hamburger").innerHTML === "Menu") {
-        document.getElementById("hamburger").innerHTML = "X";
+        document.getElementById("hamburger").innerHTML = "&#9776;";
     } else {
         document.getElementById("hamburger").innerHTML = "Menu";
     }
--- lesson8/js/longdates.js
+++ lesson8/js/longdates.js
@@ -11,12 +11,3 @@ document.getElementById('currentdate').textContent = new Date().toLocaleDateStri
 // Shows Current Year
 document.getElementById('currentyear').textContent = new Date().getFullYear();
 
-/*// Shows Friday Announcement Banner
-day = new Date().getDay();
-let announcement = document.getElementById("announcement");
-
-if (day == 5) {
-    announcement.style.display = "block";
-} else {
-    announcement.style.display = "none";
-}*/
