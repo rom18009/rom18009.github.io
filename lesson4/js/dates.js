6e6b20d459b930f0f373fe57df8f89f6569dd45e
rom18009 <rom18009@byui.edu>
2021-01-30 15:36:04 -0700

    lesson4 local to github

---
+++ lesson4/js/dates.js
@@ -0,0 +1,12 @@
+const options = {
+    weekday: 'long',
+    day: 'numeric',
+    month: 'long',
+    year: 'numeric'
+};
+document.getElementById('currentDate').textContent = new Date().toLocaleDateString('en-GB', options);
+
+// document.getElementById('currentYear').textContent = new Date().getFullYear();
+
+// Shows Last Modified Date
+// document.getElementById('lastUpdated').innerHTML = document.lastModified;
