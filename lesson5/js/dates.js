f0db074444c4609656dc1151688fac4a54a17f85
rom18009 <rom18009@byui.edu>
2021-02-06 16:31:10 -0700

    local save to github

---
+++ lesson5/js/dates.js
@@ -0,0 +1,24 @@
+// Shows Last Modified Date
+// document.getElementById('lastUpdated').innerHTML = document.lastModified;
+
+const options = {
+    weekday: 'long',
+    day: 'numeric',
+    month: 'long',
+    year: 'numeric'
+};
+document.getElementById('currentDate').textContent = new Date().toLocaleDateString('en-GB', options);
+
+// // Current Year
+// document.getElementById('currentYear').textContent = new Date().getFullYear();
+
+// Shows Friday Announcement Banner
+day = new Date().getDay();
+let announcement = document.getElementById("announcement");
+
+if (day == 5) {
+    announcement.style.display = "block";
+} else {
+    announcement.style.display = "none";
+}
+
