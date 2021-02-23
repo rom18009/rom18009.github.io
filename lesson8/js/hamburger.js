09443e32eaa33f28eb446a667de23d4c31ca8f5f
rom18009 <rom18009@byui.edu>
2021-02-22 20:26:23 -0700

    start of lesson8

---
+++ lesson8/js/hamburger.js
@@ -0,0 +1,12 @@
+// Small view hamburger menu
+const hambutton = document.querySelector('.hamburger');
+const navbar = document.querySelector('.navbar')
+
+hambutton.addEventListener('click', () => {
+    navbar.classList.toggle('responsive')
+}, false);
+
+// To solve the mid resizing issue with responsive class on
+window.onresize = () => {
+    if (window.innerWidth > 760) navbar.classList.remove('responsive')
+};
