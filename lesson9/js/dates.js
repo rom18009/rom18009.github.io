108cbbf90b7023e1468b467407442d008750db5d
rom18009 <rom18009@byui.edu>
2021-03-02 19:47:48 -0700

    lesson 9 work

---
+++ lesson9/js/dates.js
@@ -0,0 +1,23 @@
+// Shows Last Modified Date
+// document.getElementById('lastUpdated').innerHTML = document.lastModified;
+
+const options = {
+    weekday: 'long',
+    day: 'numeric',
+    month: 'long',
+    year: 'numeric'
+};
+document.getElementById('currentDate').textContent = new Date().toLocaleDateString('en-GB', options);
+
+// // Current Year
+// document.getElementById('currentYear').textContent = new Date().getFullYear();
+
+// Shows Friday Announcement Banner if Day equals 5
+day = new Date().getDay();
+let announcement = document.getElementById("announcement");
+
+if (day == 5) {
+    announcement.style.display = "block";
+} else {
+    announcement.style.display = "none";
+}
