c1551c5067a69ae5c9d20dd7db3122534f5cbfdb
rom18009 <rom18009@byui.edu>
2021-03-18 23:22:48 -0600

    adjusted rating to go to 10

--- lesson11/js/form.js
+++ lesson11/js/form.js
@@ -6,7 +6,7 @@ function adjustRating(rating) {
 
 function selectResponse() {
 	const s = document.querySelector('#selected')
-	const sel = document.querySelector('#regions');
+	const sel = document.querySelector('#region');
 	s.style.display = "block";
 	s.textContent = sel.value;
 
